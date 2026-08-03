b908bdf03c9048480b5450a5b7eafb768e33f9ae
Bharat <brai65917@gmail.com>
2026-08-03 20:31:38 +0530

    day 15: implement item retrieval endpoints with SQLModel

--- shop-management-api/routes/item.py
+++ shop-management-api/routes/item.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter , Path , HTTPException
 from datetime import date
-from validators.item import ItemRequest
+from sqlmodel import select
+from validators.item import ItemRequest, ItemResponse
 from database.session import get_session
 from models.models import Item 
 
@@ -23,3 +24,37 @@ def addItem(item:ItemRequest):
     session.refresh(db_item)
 
     return db_item
+
+@app.get("/items", response_model= list[ItemResponse])
+def getItems():
+    statement = select(Item)
+    session = get_session()
+    result = session.exec(statement)
+    
+    items = result.all()
+    
+    return items
+
+@app.get("/items/{id}", response_model= ItemResponse)
+def getItemById(id :int = Path(description="enter id")):
+    statement = select(Item).where(Item.item_id == id)
+    
+    session = get_session()
+    
+    result=session.exec(statement)
+    
+    item = result.first()
+    
+    if item is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found"
+        )
+
+
+    
+    return item
+    
+    
+    
+    
--- shop-management-api/validators/item.py
+++ shop-management-api/validators/item.py
@@ -5,3 +5,10 @@ class ItemRequest(BaseModel):
     mrp : int = Field(description="in paise" , ge = 100 )
     cost_price :int = Field(description="in paise" , ge = 20)
     stock : int = Field(ge=0)
+    
+class ItemResponse(BaseModel):
+    item_id : int 
+    item_name : str 
+    mrp : int
+    cost_price : int 
+    stock : int
